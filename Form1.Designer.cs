2bab42c2333f01edc0014c080bbf12916a2c1356
Serafim Petrov <serafimspetrovs@gmail.com>
2023-12-07 20:24:22 +0200

    [master]: Project upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace manager
+{
+    partial class mane_panel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+       private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mane_panel));
+            this.leftpanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.phonenumber = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.citata = new System.Windows.Forms.TextBox();
+            this.exit = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.leftpanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftpanel
+            // 
+            this.leftpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(35)))), ((int)(((byte)(39)))));
+            this.leftpanel.Controls.Add(this.pictureBox1);
+            this.leftpanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftpanel.Location = new System.Drawing.Point(0, 0);
+            this.leftpanel.Name = "leftpanel";
+            this.leftpanel.Size = new System.Drawing.Size(150, 450);
+            this.leftpanel.TabIndex = 1;
+           // this.leftpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.leftpanel_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // phonenumber
+            // 
+            this.phonenumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(35)))), ((int)(((byte)(39)))));
+            this.phonenumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phonenumber.Font = new System.Drawing.Font("Cascadia Code", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phonenumber.ForeColor = System.Drawing.Color.Wheat;
+            this.phonenumber.Location = new System.Drawing.Point(380, 235);
+            this.phonenumber.MaxLength = 12;
+            this.phonenumber.Multiline = true;
+            this.phonenumber.Name = "phonenumber";
+            this.phonenumber.Size = new System.Drawing.Size(324, 56);
+            this.phonenumber.TabIndex = 3;
+            this.phonenumber.TextChanged += new System.EventHandler(this.phonenumber_TextChanged);
+            this.phonenumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.phonenumber_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(47)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Cascadia Code", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Wheat;
+            this.textBox2.Location = new System.Drawing.Point(183, 243);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(182, 56);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Pers. kods";
+           // this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // citata
+            // 
+            this.citata.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(47)))));
+            this.citata.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.citata.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.citata.ForeColor = System.Drawing.Color.Wheat;
+            this.citata.Location = new System.Drawing.Point(183, 46);
+            this.citata.Multiline = true;
+            this.citata.Name = "citata";
+            this.citata.ReadOnly = true;
+            this.citata.Size = new System.Drawing.Size(563, 115);
+            this.citata.TabIndex = 7;
+            this.citata.TextChanged += new System.EventHandler(this.citata_TextChanged);
+            // 
+            // exit
+            // 
+            this.exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit.Font = new System.Drawing.Font("Cascadia Code", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.SystemColors.Menu;
+            this.exit.Location = new System.Drawing.Point(728, -18);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(100, 79);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "x";
+            this.exit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            this.exit.MouseEnter += new System.EventHandler(this.exit_MouseEnter);
+            this.exit.MouseLeave += new System.EventHandler(this.exit_MouseLeave_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 180000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(47)))));
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.password.Font = new System.Drawing.Font("Cascadia Code", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.ForeColor = System.Drawing.Color.Wheat;
+            this.password.Location = new System.Drawing.Point(183, 327);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.ReadOnly = true;
+            this.password.Size = new System.Drawing.Size(461, 79);
+            this.password.TabIndex = 9;
+            this.password.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 26.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(533, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 59);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Pieslēgties";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseEnter += new System.EventHandler(this.label1_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            // 
+            // mane_panel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.citata);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.phonenumber);
+            this.Controls.Add(this.leftpanel);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mane_panel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mane_panel_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mane_panel_MouseMove);
+            this.leftpanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel leftpanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox phonenumber;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox citata;
+        private System.Windows.Forms.Label exit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace manager
+{
+    public partial class mane_panel : Form
+    {
+        private Point lastPoint;
+
+        public mane_panel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            citata_TextChanged(this, EventArgs.Empty);
+            timer1.Start();
+        }
+
+        private void citata_TextChanged(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+
+            string selectedQuote = "";
+
+            switch (randomNumber)
+            {
+                case 1:
+                    selectedQuote = "\"Neviens cits neizdarīs tavu darbu tāpat labi kā tu pats.\" - Jānis Čakste";
+                    break;
+
+                case 2:
+                    selectedQuote = "\"Mācies visu laiku, jo tas ir tikai iespēja. Rītdien var būt jau par vēlu.\" - Juris Kaukulis";// //
+                    break;
+
+                case 3:
+                    selectedQuote = "\"Doma bez darba ir kā putns bez spārniem.\" - Jānis Zālītis";//
+                    break;
+
+                case 4:
+                    selectedQuote = "\"Dzīvot iespējams dažādi. Bet visi dzīvo mūžīgo leģendu paliekamību.\" - Andrejs Pumpurs";// 
+                    break;
+
+                case 5:
+                    selectedQuote = "\"Vēlējumos ir slēpjas jūsu nākotne. Ja jums ir griba, jums ir viss, kas nepieciešams.\" - Māris Riekstiņš";
+                    break;
+
+                case 6:
+                    selectedQuote = "\"Dzīve ir tāda, kāda ir mūsu domas.\" - Māris Čaklais";//
+                    break;
+
+                case 7:
+                    selectedQuote = "\"Dzīve ir piedzīvojums, kuram jābūt dzīvam, nevis jāsapņo.\" - Māris Čaklais";//
+                    break;
+
+                case 8:
+                    selectedQuote = "\"Dzīve bez mērķa ir kā ceļojums bez galamērķa.\" - Artis Pabriks";
+                    break;
+                case 9:
+                    selectedQuote = "\"Dzīve nav par to, kā sagaidīt vētru, bet kā dejot lietū.\" - Senā latviešu sakāmvārds";
+                    break;
+                case 10:
+                    selectedQuote = "\"Dzīve ir mācību process, un izglītība dara mūs bagātākus.\" - Māris Čaklais";//
+                    break;
+                case 11:
+                    selectedQuote = "\"Izglītība ir tas, kas paliek pēc tam, kad aiziet visi citi.\" - Elīna Garanča";
+                    break;
+
+
+            }
+
+            citata.Text = selectedQuote;
+        }
+
+        private void phonenumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+            if (phonenumber.Text.Length == 6 && e.KeyChar != (char)Keys.Back)
+            {
+                phonenumber.Text += "-";
+                phonenumber.SelectionStart = phonenumber.Text.Length;
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exit_MouseHover(object sender, EventArgs e)
+        {
+            exit.ForeColor = Color.Green;
+        }
+
+        private void exit_MouseLeave(object sender, EventArgs e)
+        {
+            exit.ForeColor = Color.White;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private string FindInformationByPhoneNumber(string phoneNumber)
+        {
+            string fileName = "stud.txt";
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, fileName);
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    foreach (string line in lines)
+                    {
+                        if (line.Contains(phoneNumber))
+                        {
+                            int index = line.IndexOf(phoneNumber);
+                            if (index >= 0)
+                            {
+                                return line.Substring(index + phoneNumber.Length);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл не найден по пути: " + filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при чтении файла: " + ex.Message);
+            }
+
+            return "Error";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = phonenumber.Text;
+
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                string information = FindInformationByPhoneNumber(phoneNumber);
+                if (information != "Error")
+                {
+                    password.Text = "Parole: " + information;
+                }
+                else
+                {
+                    password.Text = "Šāds personas kods nav atrasts.";
+                }
+            }
+            else
+            {
+                password.Text = "Lūdzu, ievadiet savu personas kodu.";
+            }
+        }
+
+        private void phonenumber_TextChanged(object sender, EventArgs e)
+        {
+            if (phonenumber.Text.Length == 6)
+            {
+                phonenumber.Text += "-";
+                phonenumber.SelectionStart = phonenumber.Text.Length;
+            }
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Green;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.White;
+        }
+
+        private void exit_MouseEnter(object sender, EventArgs e)
+        {
+            exit.ForeColor = Color.Red;
+        }
+
+        private void exit_MouseLeave_1(object sender, EventArgs e)
+        {
+            exit.ForeColor = Color.White;
+        }
+
+        private void mane_panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void mane_panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+    }
+}
